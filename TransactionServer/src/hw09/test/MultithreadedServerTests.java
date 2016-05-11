770f2a9fedbba2967973fe140c59215632f2783b
jplhanna <jpl.hanna@gmail.com>
2016-05-11 18:23:29 -0400

    added last set of tests

--- TransactionServer/src/hw09/test/MultithreadedServerTests.java
+++ TransactionServer/src/hw09/test/MultithreadedServerTests.java
@@ -92,13 +92,46 @@ public class MultithreadedServerTests extends TestCase {
 		accounts[8]=new Account(26);//i
 		accounts[9]=new Account(360);//j
 		accounts[21]=new Account(450);//v
-		accounts[22]=new Account(10);//v
+		accounts[22]=new Account(10);//w
 		
 		MultithreadedServer.runServer("src/hw09/data/referenceData", accounts);
 		
-		assertEquals("Reference of A does not work",accounts[A],accounts[2]);
-		assertEquals("Double reference of G does not work",accounts[5],accounts[8]);
-		assertEquals("Reference subtraction might not work",47,accounts[7]);
-		assertEquals("Reference subtraction might not work",58,accounts[7]);
+		assertEquals("Reference of A does not work",accounts[A].getValue(),accounts[2].getValue());
+		assertEquals("Double reference of G does not work",accounts[5].getValue(),accounts[8].getValue());
+		assertEquals("Reference subtraction might not work",47,accounts[7].getValue());
+		assertEquals("Reference subtraction might not work",58,accounts[7].getValue());
+	}
+	
+	@Test
+	public void multiTransactionsTest() throws IOException{
+		accounts=new Account[numLetters];
+		
+		//initialize accounts
+				accounts=new Account[numLetters];
+				accounts[A]=new Account(0);//a
+				accounts[1]=new Account(3);//b
+				accounts[2]=new Account(5);//c
+				accounts[3]=new Account(8);//d
+				accounts[4]=new Account(0);//e
+				accounts[5]=new Account(20);//f
+				accounts[6]=new Account(10);//g
+				accounts[7]=new Account(5);//h
+				accounts[8]=new Account(0);//i
+				accounts[9]=new Account(40);//j
+				accounts[10]=new Account(2);//k
+				accounts[11]=new Account(8);//l
+				accounts[12]=new Account(0);//m
+				accounts[13]=new Account(300);//n
+				accounts[14]=new Account(200);//o
+				accounts[15]=new Account(100);//p
+				accounts[18]=new Account(50);//s
+				accounts[22]=new Account(125);//w
+		
+		MultithreadedServer.runServer("src/hw09/data/multiTransactions",accounts);
+		
+		assertEquals("Tri addition differs",16,accounts[A].getValue());
+		assertEquals("Tri subtraction differs",5,accounts[4].getValue());
+		assertEquals("Mixed evaluation differs",46,accounts[8].getClass());
+		assertEquals("Multi reference evaluation differs",125,accounts[12].getValue());
 	}
 }
